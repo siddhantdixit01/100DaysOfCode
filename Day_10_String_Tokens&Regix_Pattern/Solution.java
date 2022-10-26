0b687c59cdb98f3aad460ba2fa6128ac4db8edea
siddhantdixit01 <114592704+siddhantdixit01@users.noreply.github.com>
2022-10-26 10:27:51 +0530

    Update Solution.java

--- Day_10_String_Tokens&Regix_Pattern/Solution.java
+++ Day_10_String_Tokens&Regix_Pattern/Solution.java
@@ -6,12 +6,16 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-        String[] arr = s.split("[ !,?._'@]+");
-        int n=arr.length;
-        System.out.println(n);
-        for(int i=0;i<n;i++){
-            System.out.println(arr[i]);
+        String[] arr = s.trim().split("()[!,?._'@ ]+");
+         if(s.trim().isEmpty()){
+        System.out.println("0");
         }
+        else{
+        System.out.println(arr.length);
+        for (String a : arr) {
+            System.out.println(a);
+        }
+        }       
         scan.close();
     }
 }
