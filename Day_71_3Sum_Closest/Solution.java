253e46fdc5c4824cd31849d7c26a3d3578773972
Siddhant Dixit <114592704+siddhantdixit01@users.noreply.github.com>
2023-01-16 22:36:53 +0530

    16/01/2023

---
+++ Day_71_3Sum_Closest/Solution.java
@@ -0,0 +1,50 @@
+/*
+3Sum Closest || LeetCode
+
+Example 1:
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+
+Example 2:
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+*/
+
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = nums[0] + nums[1] + nums[nums.length - 1];
+        int closestSum = sum;
+
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i==0 || nums[i]!=nums[i-1]){
+                int left = i + 1, right = nums.length - 1;
+                while(left < right){
+                    sum = nums[left] + nums[right] + nums[i];
+                    if(sum < target){
+                        while(left<right && nums[left] == nums[left+1]){
+                            left++;
+                        }
+                        left++;
+                    }else if(sum > target){
+                        while(left<right && nums[right] == nums[right-1]){
+                            right--;
+                        }
+                        right--;
+                    }else{
+                        return sum;
+                    }
+                    if(Math.abs(target - sum) < Math.abs(target - closestSum)){
+                        closestSum = sum;
+                    }
+                }
+            }
+
+        }
+        return closestSum;
+    }
+}
