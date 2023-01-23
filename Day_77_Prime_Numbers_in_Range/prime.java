8c7e7681933ab5a244708a11137fb918fc66b745
Siddhant Dixit <114592704+siddhantdixit01@users.noreply.github.com>
2023-01-23 21:47:10 +0530

    23/01/2023

---
+++ Day_77_Prime_Numbers_in_Range/prime.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class prime {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        boolean[] b=new boolean[n+1];
+        for(int i=0;i<=n;i++){
+            b[i]=true;
+        }
+        for(int i=2; i*i<=n;i++)
+            if(b[i]==true)
+                for(int j=i*i; j<=n;j=j+i)
+                    b[j]=false;
+        for(int i=1;i<=n;i++){
+            if(b[i]==true)
+                System.out.print(i+" ");
+        }
+    }
+}
